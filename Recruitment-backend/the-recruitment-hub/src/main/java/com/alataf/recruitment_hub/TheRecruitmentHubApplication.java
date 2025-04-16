f206e377fd974db40b53c22f054e2a45c6776202
meAltf <alataf.dce.cs1@gmail.com>
2025-04-16 22:49:31 +0530

    backend initial set-up

---
+++ Recruitment-backend/the-recruitment-hub/src/main/java/com/alataf/recruitment_hub/TheRecruitmentHubApplication.java
@@ -0,0 +1,13 @@
+package com.alataf.recruitment_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRecruitmentHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRecruitmentHubApplication.class, args);
+	}
+
+}
---
+++ Recruitment-backend/the-recruitment-hub/src/test/java/com/alataf/recruitment_hub/TheRecruitmentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alataf.recruitment_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRecruitmentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
